8c77df1dac6276422723868652ac8d1d374abc86
sahargholami1992 <sahar_gholami03@gmail.com>
2024-02-20 01:55:21 +0330

    delete this classes

--- src/main/java/com/example/final_project_4/service/PaymentService.java
+++
@@ -1,8 +0,0 @@
-package com.example.final_project_4.service;
-
-
-import com.example.final_project_4.dto.PaymentRequest;
-
-public interface PaymentService {
-    void paymentDone(PaymentRequest request);
-}
